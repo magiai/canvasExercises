aaea43c745957557f3745fe34f14b12f12e4c212
Dominika <adamskdominik@gmail.com>
2018-01-08 16:26:57 +0100

    circles move in the different directions

--- canvas.js
+++ canvas.js
@@ -48,35 +48,63 @@ var c = canvas.getContext('2d');//contact
 // }
 
 
-c.beginPath();
-c.arc(200, 200, 30, 0, Math.PI * 2, false);
-c.strokeStyle = "red";
-c.stroke();
 
-var x = Math.random() * innerWidth;
-var y = Math.random() * innerHeight;
-var dx = (Math.random() - 0.5) * 8;
-var dy = (Math.random() - 0.5) * 8;
-var radius = 30;
+function Circle(x, y, dx, dy, radius) {
+    this.x = x;
+    this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.radius = radius;
+
+    this.draw = function() {
+
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        c.strokeStyle = "red";
+        c.stroke();
+    }
+
+    this.update = function () {
+
+        if (this.x + this.radius > innerWidth ||
+            this.x - this.radius < 0) {
+            this.dx = -this.dx;
+        }
+        if (this.y + this.radius > innerWidth ||
+            this.y - this.radius < 0) {
+            this.dy = -this.dy;
+        }
+
+        this.x += this.dx; //żeby kółko przemieszczało się
+        this.y += this.dy;
+
+        this.draw();
+    }
+}
+
+
+var circleArray = [];
+
+for (var i = 0; i < 100; i++) {
+    var radius = 30;
+    var x = Math.random() * innerWidth;
+    var y = Math.random() * innerHeight;
+    var dx = (Math.random() - 0.5) * 3;
+    var dy = (Math.random() - 0.5) * 3;
+
+    circleArray.push(new Circle(x, y, dx, dy, radius));
+}
+
 function animate() {
     requestAnimationFrame(animate);
     c.clearRect(0, 0, innerWidth, innerHeight);
 
-    c.beginPath();
-    c.arc(x, y, 30, 0, Math.PI * 2, false);
-    c.strokeStyle = "violet";
-    c.stroke();
-
-    if (x + radius > innerWidth || x - radius < 0) {
-        dx = -dx;
-    }
-    if (y + radius > innerWidth || y - radius < 0) {
-        dy = -dy;
+    for (var i = 0; i < circleArray.length; i++) {
+        circleArray[i].update();
     }
 
+    // circle.update();
 
-    x += dx; //żeby kółko przemieszczało się
-    y += dy;
 }
 
 animate();
