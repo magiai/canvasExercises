d0c68904644e233074319755fb553e2be6ae11b1
Dominika <adamskdominik@gmail.com>
2018-01-10 19:25:49 +0100

    curves

--- exercise2/drawing curves/canvas.js
+++ exercise2/drawing curves/canvas.js
@@ -1,7 +1,27 @@
-window.onload = function () {
+var canvas = document.querySelector('canvas');
 
-    var canvas = document.getElementById("hello-world-canvas");
-    var context = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
+var context = canvas.getContext("2d");
 
-}
+//arc(x, y, radius, startAngle, endAngle, counterClockWise)
+var radian = Math.PI / 180;
+
+context.beginPath();
+context.strokeStyle = "orange";
+context.lineWidth = 10;
+context.arc(200, 100, 100, 0 * radian, 180 * radian, false);
+context.stroke();
+
+context.beginPath();
+context.strokeStyle = "green";
+context.lineWidth = 10;
+context.arc(300, 200, 100, 45 * radian, 180 * radian, false);
+context.stroke();
+
+context.beginPath();
+context.strokeStyle = "brown";
+context.lineWidth = 10;
+context.arc(300, 200, 100, 45 * radian, 120 * radian, true);
+context.stroke();
