b4bf70dc39d09964a581e947b617e996a58a31af
Dominika <adamskdominik@gmail.com>
2018-01-13 11:41:37 +0100

    text styling

--- exercise2/text/canvas.js
+++ exercise2/text/canvas.js
@@ -5,18 +5,25 @@ canvas.height = window.innerHeight;
 
 var context = canvas.getContext("2d");
 
-context.font = "90px Helvetica";
+context.font = "50px Helvetica";
 
 //context.fillText(text, xCoord, yCoord, maxWidth);
 
 var fText = "Test tekst teks";
 context.fillText(fText, 400, 200);
 
-// context.beginPath();
-// context.strokeStyle = black;
-// context.lineWidth = 5;
-// context.arc(500, 200, 100, 0 * (Math.PI / 180), 360 * (Math.PI / 180), false);
-// context.stroke();
-
 var sText = "Nananana";
-context.strokeText(sText, 400, 400);
+context.strokeText(sText, 400, 300);
+
+//context.font = "font-style font-weight font-size font-family";
+
+var text = "Stages of ODCs formation";
+context.font = "normal bolder xx-large Monospace";
+context.strokeText(text, 400, 400);
+
+var text2 = "Plasticity and the role " +
+    "of experience in neurocortical development";
+context.font = "italic bold 60px Helvetica";
+context.fillText(text2, 400, 500);
+
+
